79a44f4d8a3a236209bc99ca1e9214539c6c4f2f
tylermanguy58 <tylerstricklandwork@gmail.com>
2025-12-01 09:13:13 -0500

    lightwater project added, with c++ standard header replacing c standard

---
+++ code/C++/03.1.1_lightwater/lightwater.cc
@@ -0,0 +1,65 @@
+//
+// Filename:    lightwater.c
+// Description: use ten segment LED bar component
+// Components:  LED bar
+//
+////////////////////////////////////////////////////////////////////////////////////////
+
+// Pre-processor directives
+#include <wiringPi.h>
+#include <iostream>
+#include <cstdint>
+
+#define  NUM_LEDS  10
+
+int32_t  main (void)
+{
+    // Declare and initialize variables
+    int32_t  position     = 0;
+    int32_t  LEDbar[]     = { 0, 1, 2, 3, 4, 5, 6, 8, 9, 10 }; // wiringPi GPIO pins
+
+    // Bring wiringPi functionality online
+    if (wiringPiSetup () == -1)
+    {
+        std::cout << "[ERROR] Could not initialize wiringPi library!" << std::endl;
+        exit (1);
+    }
+
+    // initialize each LEDbar wiringPi pin to OUTPUT mode
+    for (position = 0; position < NUM_LEDS; position++)
+    {
+        pinMode (LEDbar[position], OUTPUT);
+    }
+
+    std::cout << "Program is starting (CTRL-c to interrupt) ..." << std::endl;
+
+    while (1)
+    {
+        ////////////////////////////////////////////////////////////////////////////////
+        //
+        // Perform a left to right display motion across the LEDbar (light up position
+        // 0 through 9 (ten positions), one at a time
+        //
+        for (position = 0; position < NUM_LEDS; position++)
+        {
+            digitalWrite (LEDbar[position], LOW);
+            delay (100);
+            digitalWrite (LEDbar[position], HIGH);
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////
+        //
+        // Perform a right to left display motion across the LEDbar (light up position
+        // 0 through 9 (ten positions), one at a time
+        //
+        for (position = NUM_LEDS - 1; position >= 0; position--)
+        {
+            digitalWrite (LEDbar[position], LOW);
+            delay (100);
+            digitalWrite (LEDbar[position], HIGH);
+        }
+    }
+
+    return (0);
+}
+
