a6e943a95bf939073d73de534c842f2aabc6ad42
tylermanguy58 <tylerstricklandwork@gmail.com>
2025-12-01 08:56:48 -0500

    blink led is pointer, so fixed method calls using that pointer that were using . rather than ->

--- code/C++/01.1.1_blink/blink.cc
+++ code/C++/01.1.1_blink/blink.cc
@@ -8,10 +8,11 @@
 #include <iostream>
 #include <wiringPi.h>
 #include <LED.h>                                     // libLED-provided header
+#include <cstdint>
 
 #define LEDpin 0                                     // LED wiringPi pin number
 
-int main (void) {
+int32_t main (void) {
 
     // Declare and initialize variables
     LED *blink = NULL;
@@ -26,29 +27,29 @@ int main (void) {
     // create and initialize a new LED instance (called `blink`)
     blink = new LED (LEDpin, LOW);
 
-    fprintf (stdout, "Program is starting (CTRL-c to interrupt ...\n");
-    fprintf (stdout, "[LED] configured for OUTPUT mode using pin: %d\n", blink.getPin ());
+    std::cout << "Program is starting (CTRL-c to interrupt) ..." << std::endl;
+    fprintf (stdout, "[LED] configured for OUTPUT mode using pin: %d\n", blink->getPin());
 
     // Display the default status (note the lack of newline at the end, so that with
     // the strategic use of backspaces, we can update this line in place)
-    fprintf (stdout, "\nLED: OFF (%4s)", blink.getState());
+    fprintf (stdout, "\nLED: OFF (%4s)", blink->getState());
 
     while (1) {
 
         // Turn LED on
-        blink.write (HIGH);
+        blink->write (HIGH);
 
         // update information display to report current LED state
-        fprintf (stdout, "\b\b\b\b\b\b\b\b\b\bON  (%4s)", blink.getState());
+        fprintf (stdout, "\b\b\b\b\b\b\b\b\b\bON  (%4s)", blink->getState());
 
         // strategic delay for user perception (1000mS == 1 second)
         delay (1000);
 
         // Turn LED off
-        blink.write (LOW);
+        blink->write (LOW);
 
         // update information display to report current LED state
-        fprintf (stdout, "\b\b\b\b\b\b\b\b\b\bOFF (%4s)", blink.getState ());
+        fprintf (stdout, "\b\b\b\b\b\b\b\b\b\bOFF (%4s)", blink->getState());
 
         // strategic delay for user perception (1000mS == 1 second)
         delay (1000);
