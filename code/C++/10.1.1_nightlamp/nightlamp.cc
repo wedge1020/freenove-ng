813aef32481f23c32c88cc802d7b35ca5c39bd9e
Quentin Lewis <qlewis@lab46.g7n.org>
2025-12-03 16:10:16 -0500

    Copied directory over to C++, updated Makefile to C++, will work on updating the .cc file later

---
+++ code/C++/10.1.1_nightlamp/nightlamp.cc
@@ -0,0 +1,47 @@
+//
+// Filename:    nightlamp.cc
+// Description: Nightlamp - use photoresistor to control LED.
+// NOTE:        needs ADCDevice library from code/C/libraries/ADCDevice
+//
+////////////////////////////////////////////////////////////////////////////////////////
+
+// Pre-processor directives
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <softPwm.h>
+#include <ADCDevice.h>
+
+#define  LEDpin 0
+
+// Declare Global ADCDevice variable
+ADCDevice *adc;
+
+int32_t  main (void)
+{
+    // Declare and initialize variables
+    int32_t    ADCvalue  = 0;
+    float      voltage   = 0.0;
+
+    // Initialize the ADCDevice library
+    adc                  = NULL;
+    init_ADCDevice ();
+
+    // Create software PWM control for LED
+    softPwmCreate (LEDpin, 0, 100);
+
+    fprintf (stdout, "Program is starting (CTRL-c to interrupt) ...\n");
+
+    // Main loop: proceed indefinitely until interruption
+    while (1)
+    {
+        ADCvalue         = adc -> analogRead (0);          // read value of A0 pin
+        softPwmWrite (LEDpin, ADCvalue * 100 / 255);       // Mapping to PWM duty cycle
+        voltage          = (float) ADCvalue / 255.0 * 3.3; // Calculate voltage
+        fprintf (stdout, "ADC value: %3d, Voltage: %.2fV\n", ADCvalue, voltage);
+        delay (100);
+    }
+
+    return (0);
+}
