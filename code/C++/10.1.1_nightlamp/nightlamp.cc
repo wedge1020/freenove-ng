d582e18115cf2a6f1e3469ee7ce120dafbd502db
Matthew Haas <haas@corning-cc.edu>
2025-12-07 17:16:33 -0500

    C++/10.1.1 more fixes

--- code/C++/10.1.1_nightlamp/nightlamp.cc
+++ code/C++/10.1.1_nightlamp/nightlamp.cc
@@ -16,18 +16,35 @@
 
 #define  LEDpin 0
 
-// Declare Global ADCDevice variable
-ADCDevice *adc;
-
 int32_t  main (void)
 {
     // Declare and initialize variables
+    ADCDevice *adc       = NULL;
     int32_t    ADCvalue  = 0;
     float      voltage   = 0.0;
 
     // Initialize the ADCDevice library
-    adc                  = NULL;
-    init_ADCDevice ();
+    adc                  = new ADCDevice ();
+
+    // Detect the pcf8591.
+    if (adc -> detectI2C (0x48))
+    {
+        delete adc;              // Free previously pointed memory
+        adc = new PCF8591();     // If detected, create an instance of PCF8591.
+    }
+    // Detect the ads7830
+    else if (adc -> detectI2C (0x4b))
+    {
+        delete adc;               // Free previously pointed memory
+        adc = new ADS7830();      // If detected, create an instance of ADS7830.
+    }
+    else
+    {
+        fprintf (stderr, "No correct I2C address found, \n"
+        "Please use command 'i2cdetect -y 1' to check the I2C address! \n"
+        "Program Exit. \n");
+        exit (1);
+    } 
 
     // Create software PWM control for LED
     softPwmCreate (LEDpin, 0, 100);
@@ -40,8 +57,11 @@ int32_t  main (void)
         ADCvalue         = adc -> analogRead (0);          // read value of A0 pin
         softPwmWrite (LEDpin, ADCvalue * 100 / 255);       // Mapping to PWM duty cycle
         voltage          = (float) ADCvalue / 255.0 * 3.3; // Calculate voltage
-        std::cout << "ADC value: " << ADCvalue << ", Voltage: " << voltage << "V" << std::endl;
-		delay (100);
+        std :: cout << "ADC value: "
+                    << ADCvalue << ", Voltage: "
+                    << voltage  << "V"
+                    << std :: endl;
+        delay (100);
     }
 
     return (0);
