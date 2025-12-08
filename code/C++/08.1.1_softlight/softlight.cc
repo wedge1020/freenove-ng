7beab95df7b700411e6e81cbed95cad14f1fc71e
Matthew Haas <haas@corning-cc.edu>
2025-12-08 11:51:48 -0500

    code/C++/08.1.1 Makefile and code updated, now compiles

--- code/C++/08.1.1_softlight/softlight.cc
+++ code/C++/08.1.1_softlight/softlight.cc
@@ -1,7 +1,7 @@
 //
 // Filename:    softlight.cc
 // Description: Use potentiometer to control LED
-// NOTE:        needs ADCDevice library from code/C/libraries/ADCDevice
+// NOTE:        needs ADCDevice library from code/C++/libraries/ADCDevice
 // Components:  ADC module, potentiometer, LED
 //
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -15,18 +15,35 @@
 
 #define  LEDpin 0
 
-// Declare Global ADCDevice variable
-ADCDevice   *adc;
-
 int32_t  main (void)
 {
     // Declare and initialize variables
-    int32_t  ADCvalue    = 0;
-    float    voltage     = 0.0;
+    ADCDevice *adc       = NULL;
+    int32_t    ADCvalue  = 0;
+    float      voltage   = 0.0;
 
     // Initialize the ADCDevice library
-    adc                  = NULL;
-    init_ADCDevice ();
+    adc                  = new ADCDevice ();
+
+    // Detect the pcf8591.
+    if (adc -> detectI2C (0x48))
+    {
+        delete adc;                       // Free previously pointed memory
+        adc              = new PCF8591(); // if detected, create an instance of PCF8591
+    }
+    // Detect the ads7830
+    else if (adc -> detectI2C (0x4b))
+    {
+        delete adc;                       // Free previously pointed memory
+        adc              = new ADS7830(); // If detected, create an instance of ADS7830
+    }
+    else
+    {
+        fprintf (stderr, "No correct I2C address found, \n"
+        "Please use command 'i2cdetect -y 1' to check the I2C address! \n"
+        "Program Exit. \n");
+        exit (1);
+    } 
 
     // Create software PWM control for LED
     softPwmCreate (LEDpin, 0, 100);
