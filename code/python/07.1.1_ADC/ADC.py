437467372a2c7a42e4e61a29758def6934169e4d
Monti Emery Jr <memeryjr@corning-cc.edu>
2025-12-09 14:05:10 -0500

    Cleaned up and added comments to ADCDevice.py and ADC.py

--- code/python/07.1.1_ADC/ADC.py
+++ code/python/07.1.1_ADC/ADC.py
@@ -1,21 +1,49 @@
 #!/usr/bin/env python3
-########################################################################
+########################################################################################
 # Filename    : ADC.py
 # Description : Use ADC module to read the voltage value of potentiometer.
 # Author      : www.freenove.com
 # modification: 2023/05/11
-########################################################################
+########################################################################################
+
+########################################################################################
+##
+## import the `time` lib and import the ADCDevice module
+##
 import time
 from ADCDevice import *
 
-adc = ADCDevice() # Define an ADCDevice class object
+########################################################################################
+##
+## instantiating the ADCDevice class
+##
+adc = ADCDevice()
 
+########################################################################################
+##
+## sets up the adc object
+##
 def setup():
+
+    ####################################################################################
+    ##
+    ## using the global adc object
+    ##
     global adc
+    
+    ####################################################################################
+    ##
+    ## detect the type of ADC then instantiate the detected device class
+    ##
     if(adc.detectI2C(0x48)): # Detect the pcf8591.
         adc = PCF8591()
     elif(adc.detectI2C(0x4b)): # Detect the ads7830
         adc = ADS7830()
+    
+    ####################################################################################
+    ##
+    ## if no device is detected, display the message below and exit the program
+    ##
     else:
         print("No correct I2C address found, \n"
         "Please use command 'i2cdetect -y 1' to check the I2C address! \n"
@@ -24,20 +52,63 @@ def setup():
         
 def loop():
     while True:
-        value = adc.analogRead(0)    # read the ADC value of channel 0
-        voltage = value / 255.0 * 3.3  # calculate the voltage value
+    
+        ################################################################################
+        ##
+        ## read the ADC value of channel 0
+        ##
+        value = adc.analogRead(0)
+        
+        ################################################################################
+        ##
+        ## calculate the voltage value
+        ##
+        voltage = value / 255.0 * 3.3
+        
+        ################################################################################
+        ##
+        ## display the ADC value and voltage
+        ##
         print ('ADC Value : %d, Voltage : %.2f'%(value,voltage))
+        
+        ################################################################################
+        ##
+        ## strategic delay for user perception (0.1 seconds)
+        ##
         time.sleep(0.1)
 
 def destroy():
+
+    ####################################################################################
+    ##
+    ## close the connection to the ADC pin
+    ##
     adc.close()
     
-if __name__ == '__main__':   # Program entrance
+########################################################################################
+##
+## start of program
+##
+if __name__ == '__main__':
+
+    ####################################################################################
+    ##
+    ## display operational information
+    ##
     print ('Program is starting ... ')
+    
+    ####################################################################################
+    ##
+    ## normal operations: run the setup function and run the loop
+    ##
     try:
         setup()
         loop()
+        
+    ####################################################################################
+    ##
+    ## catch any keyboard interrupt characters to signal program exit
+    ##
     except KeyboardInterrupt: # Press ctrl-c to end the program.
         destroy()
         print("Ending program")
-        
--- code/python/07.1.1_ADC/ADCDevice.py
+++ code/python/07.1.1_ADC/ADCDevice.py
@@ -1,52 +1,107 @@
 #!/usr/bin/env python3
-########################################################################
+#############################################################################################
 # Filename    : ADCDevice.py
 # Description : Freenove ADC Module library.
 # Author      : www.freenove.com
 # modification: 2023/05/11
-########################################################################
+#############################################################################################
 
+#############################################################################################
+##
+## import the smbus library
+##
 import smbus
 
+#############################################################################################
+##
+## for storing ADCDevice values
+##
 class ADCDevice(object):
+
+    #########################################################################################
+    ##
+    ## initializes the attributes of the class
+    ##
     def __init__(self):
         self.cmd = 0
         self.address = 0
         self.bus=smbus.SMBus(1)
         # print("ADCDevice init")
         
+    #########################################################################################
+    ##
+    ## detects the device in the provided address
+    ##
     def detectI2C(self,addr):
+        
+        #####################################################################################
+        ##
+        ## detects the device and displays a conformation message
+        ##
         try:
             self.bus.write_byte(addr,0)
             print("Found device in address 0x%x"%(addr))
             return True
+            
+        #####################################################################################
+        ##
+        ## displays this message if no device is found
+        ##
         except:
             print("Not found device in address 0x%x"%(addr))
             return False
-            
+           
     def close(self):
-        self.bus.close()
         
+        #####################################################################################
+        ##
+        ## close the connection to the bus pin
+        ## 
+        self.bus.close()
+
+#############################################################################################
+##
+## for storing the values for the ADS7830 ADC Module
+##        
 class PCF8591(ADCDevice):
+
+    #########################################################################################
+    ##
+    ## initializes the attributes of the class
+    ##
     def __init__(self):
         super(PCF8591, self).__init__()
         self.cmd = 0x40     # The default command for PCF8591 is 0x40.
         self.address = 0x48 # 0x48 is the default i2c address for PCF8591 Module.
-        
+     
+    #########################################################################################
+    ##
+    ## returns the ADC value from the specified address, command, and pins
+    ##   
     def analogRead(self, chn): # PCF8591 has 4 ADC input pins, chn:0,1,2,3
         value = self.bus.read_byte_data(self.address, self.cmd+chn)
         value = self.bus.read_byte_data(self.address, self.cmd+chn)
         return value
     
-    def analogWrite(self,value): # write DAC value
-        self.bus.write_byte_data(address,cmd,value)	
-
+#############################################################################################
+##
+## for storing the values for the ADS7830 ADC Module
+##
 class ADS7830(ADCDevice):
+
+    #########################################################################################
+    ##
+    ## initializes the attributes of the class
+    ##
     def __init__(self):
         super(ADS7830, self).__init__()
         self.cmd = 0x84
         self.address = 0x4b # 0x4b is the default i2c address for ADS7830 Module.   
-        
+    
+    #########################################################################################
+    ##
+    ## returns the ADC value from the specified address, command, and pins
+    ##    
     def analogRead(self, chn): # ADS7830 has 8 ADC input pins, chn:0,1,2,3,4,5,6,7
         value = self.bus.read_byte_data(self.address, self.cmd|(((chn<<2 | chn>>1)&0x07)<<4))
         return value
