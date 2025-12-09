fc803ded1df0cee625e70476bed3993b52e5fadd
Monti Emery Jr <memeryjr@corning-cc.edu>
2025-12-09 10:35:59 -0500

    Cleaned up and added comments to Alertor.py

--- code/python/06.2.1_Alertor/Alertor.py
+++ code/python/06.2.1_Alertor/Alertor.py
@@ -1,40 +1,129 @@
 #!/usr/bin/env python3
-########################################################################
+########################################################################################
 # Filename    : Alertor.py
 # Description : Make Alertor with buzzer and button
 # Author      : www.freenove.com
 # modification: 2019/12/27
-########################################################################
+########################################################################################
+
+########################################################################################
+##
+## import `TonalBuzzer`, `Button`, and `Tone` functionality from `gpiozero` library
+##
 from gpiozero import TonalBuzzer,Button
 from gpiozero.tones import Tone
+
+########################################################################################
+##
+## import the `time` and `math` libs
+##
 import time
 import math
 
+########################################################################################
+##
+## instantiating the TonalBuzzer class on pin 17
+##
 buzzer = TonalBuzzer(17)
-button = Button(18) # define Button pin according to BCM Numbering
+
+########################################################################################
+##
+## instantiating the Button class on pin 18
+##
+button = Button(18)
 
 def loop():
     while True:
-        if button.is_pressed:  # if button is pressed
+        
+        ################################################################################
+        ##
+        ## if button is pressed
+        ##
+        if button.is_pressed:
+        
+            ############################################################################
+            ##
+            ## start playing a repeating tone
+            ##
             alertor()
+            
+            ############################################################################
+            ##
+            ## display operational information
+            ##
             print ('alertor turned on >>> ')
-        else :
+            
+        else:
+        
+            ############################################################################
+            ##
+            ## stop playing a repeating tone
+            ##
             stopAlertor()
+            
+            ############################################################################
+            ##
+            ## display operational information
+            ##
             print ('alertor turned off <<<')
+            
+########################################################################################
+##
+## start playing a repeating tone
+##            
 def alertor():
-    buzzer.play(Tone(220.0)) 
+    
+    ####################################################################################
+    ##
+    ## play a tone using the buzzer
+    ##
+    buzzer.play(Tone(220.0))
+    
+    ####################################################################################
+    ##
+    ## strategic delay for user perception (.1 seconds)
+    ##
     time.sleep(0.1)
         
 def stopAlertor():
+
+    ####################################################################################
+    ##
+    ## stop playing the buzzer tone
+    ##
     buzzer.stop()
             
 def destroy():
+    
+    ####################################################################################
+    ##
+    ## closes the connection to the buzzer pin
+    ##
     buzzer.close()                  
 
-if __name__ == '__main__':     # Program entrance
+########################################################################################
+##
+## start of program
+##
+if __name__ == '__main__':
+
+    ####################################################################################
+    ##
+    ## display operational information
+    ##
     print ('Program is starting...')
+    
+    ####################################################################################
+    ##
+    ## normal operations: run the loop
+    ##
     try:
         loop()
+        
+    ####################################################################################
+    ##
+    ## catch any keyboard interrupt characters to signal program exit
+    ##
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
         destroy()
         print("Ending program")
