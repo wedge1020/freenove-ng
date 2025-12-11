1e5319ea933b88a499c8c498af906d110807601c
nick3-git <ngugliel@corning-cc.edu>
2025-12-10 20:01:29 -0500

    Converted the lightwater python code to bash. It should constantly loop the led from left to right and right to lefdt until ctrl-c is pressed

--- code/bash/03.1.1_lightwater/lightwater.sh
+++ code/bash/03.1.1_lightwater/lightwater.sh
@@ -6,6 +6,36 @@
 # Modification: December 9, 2025
 #############################################################
 
-ledpin=(17, 18, 27, 22, 23, 24, 25, 2, 3, 8)
+declare -a ledpin=(17 18 27 22 23 24 25 2 3 8)
+declare -a backwardsled=(8 3 2 25 24 23 22 27 18 17)
 sleep="0.1"
+ON=1
+OFF=0
 
+
+#This is to make sure all lights start as off
+# and that pin is able to be used as output for the led pins
+for pin in "${ledpin[@]}"; do
+	gpio mode "$pin" out
+	gpio write "$pin" $OFF
+done
+
+echo "The program is starting, please press ctrl-c to end the program"
+
+while true; do
+	#make led go from left to right
+	for pin in "${ledpin[@]}"; do
+		gpio write "$pin" ${ON}
+		sleep $sleep
+		gpio write "$pin" ${OFF}
+	done
+	#make led go from right to left
+	for pin in "${backwardsled[@]}"; do
+		gpio write "$pin" ${ON}
+		sleep $sleep
+		gpio write "$pin" ${OFF}
+	done
+
+done
+
+exit 0	
